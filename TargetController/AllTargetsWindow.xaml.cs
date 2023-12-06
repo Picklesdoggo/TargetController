e1a39c40bf7246b24ccdb35ccfaf39da2eca956a
j_som <j_som@DESKTOP-ITV536S>
2023-12-06 08:19:20 -0500

    Add project files.

---
+++ TargetController/AllTargetsWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TargetController
+{
+    /// <summary>
+    /// Interaction logic for AllTargetsWindow.xaml
+    /// </summary>
+    public partial class AllTargetsWindow : Window
+    {
+        SerialPort serialPort;
+        public AllTargetsWindow()
+        {
+            InitializeComponent();
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM4"; // Set in Windows
+            serialPort.Open();
+
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+            serialPort.WriteLine("r");
+            while(serialPort.BytesToRead == 0)
+            {
+                // do nothing
+            }
+            String readData = serialPort.ReadLine();
+            
+            int breakPoint = 1;
+
+        }
+    }
+}
---
+++ TargetController/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TargetController
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static SerialPort serialPort;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            serialPort = new SerialPort();
+            serialPort.BaudRate = 9600;
+            serialPort.PortName = "COM4"; // Set in Windows
+            serialPort.Open();
+            serialPort.WriteTimeout = 1000;
+        }
+
+        private void btnRandom_Click(object sender, RoutedEventArgs e)
+        {
+            serialPort.WriteLine("2");
+        }
+
+        private void btnAll_Click(object sender, RoutedEventArgs e)
+        {
+            serialPort.WriteLine("1");
+            serialPort.Close();
+            AllTargetsWindow atw = new AllTargetsWindow();
+            atw.Show();
+            this.Close();
+        }
+    }
+}
