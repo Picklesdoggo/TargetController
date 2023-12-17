5cb8076c3485c5ff3803145513a6898c3f5de4e5
John Somers <j_somers89@yahoo.com>
2023-12-17 12:12:11 -0500

    Initial implementation of data grid with splits

--- TargetController/MainWindow.xaml.cs
+++ TargetController/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,30 +24,56 @@ namespace TargetController
     public partial class MainWindow : Window
     {
         static SerialPort serialPort;
+        ObservableCollection<SplitTime> splitTimes = new ObservableCollection<SplitTime>();
+        List<Ellipse> targets = new List<Ellipse>();
+        DateTime hitTimeReference;
 
         public MainWindow()
         {
             InitializeComponent();
-
             serialPort = new SerialPort();
             serialPort.BaudRate = 9600;
-            serialPort.PortName = "COM4"; // Set in Windows
-            serialPort.Open();
+            serialPort.PortName = "COM4"; // Set in Windows            
             serialPort.WriteTimeout = 1000;
+            serialPort.DataReceived += SerialPort_DataReceived;
+
+            targets.Add(target0);
+            targets.Add(target1);
+            targets.Add(target2);
+            targets.Add(target3);
+            targets.Add(target4);
+
+            grdSplits.ItemsSource = splitTimes;
         }
 
-        private void btnRandom_Click(object sender, RoutedEventArgs e)
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            serialPort.WriteLine("2");
+            string dataReceived = serialPort.ReadLine();
+
+            List<string> split = dataReceived.Split(',').ToList();
+
+            SplitTime splitTime = new SplitTime();
+            splitTime.TargetID = split[0];
+            splitTime.SensorReading = split[1];
+            splitTime.TimeHit = DateTime.Now;
+            splitTime.TimeDuration = splitTime.TimeHit - hitTimeReference;
+            hitTimeReference = DateTime.Now;
+
+            App.Current.Dispatcher.Invoke((Action)delegate // <--- HERE
+            {
+                int targetIndex = Convert.ToInt32(splitTime.TargetID);
+                targets[targetIndex].Fill = Brushes.Green;
+                splitTimes.Add(splitTime);
+                grdSplits.ScrollIntoView(grdSplits.Items[grdSplits.Items.Count - 1]);
+            });
+            
+
         }
 
-        private void btnAll_Click(object sender, RoutedEventArgs e)
+        private void Connect_Click(object sender, RoutedEventArgs e)
         {
-            serialPort.WriteLine("1");
-            serialPort.Close();
-            AllTargetsWindow atw = new AllTargetsWindow();
-            atw.Show();
-            this.Close();
+            serialPort.Open();
+            hitTimeReference = DateTime.Now;
         }
     }
 }
---
+++ TargetController/SplitTime.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace TargetController
+{
+    public class SplitTime : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DateTime timeHit;
+        private Duration timeDuration;
+        private string targetID;
+        private string sensorReading;
+        private string split
+        {
+            get
+            {
+                return timeDuration.TimeSpan.TotalSeconds.ToString();
+            }
+            set
+            {
+                split = value.ToString();
+            }
+        }
+
+        
+
+        public DateTime TimeHit
+        {
+            get { return timeHit; }
+            set
+            {
+                timeHit = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+        public Duration TimeDuration
+        {
+            get { return timeDuration; }
+            set
+            {
+                timeDuration = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+        public string Split
+        {
+            get { return split; }
+            set
+            {
+                split = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+        public string TargetID
+        {
+            get { return targetID; }
+            set
+            {
+                targetID = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+        public string SensorReading
+        {
+            get { return sensorReading; }
+            set
+            {
+                sensorReading = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged();
+            }
+        }
+
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
